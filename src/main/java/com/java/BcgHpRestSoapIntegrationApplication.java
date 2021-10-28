231e0f162fc2b116b704c82575a1c899b4c80315
khlnnvarmad <khlnnvarma1@gmail.com>
2021-10-28 19:11:54 +0530

    intial commit

--- src/main/java/com/java/BcgHpRestSoapIntegrationApplication.java
+++ src/main/java/com/java/BcgHpRestSoapIntegrationApplication.java
@@ -2,12 +2,13 @@ package com.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BcgHpRestSoapIntegrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BcgHpRestSoapIntegrationApplication.class, args);
+	ConfigurableApplicationContext configurableApplicationContext=	SpringApplication.run(BcgHpRestSoapIntegrationApplication.class, args);
 	}
 
 }
