66a83a71ca28cbd2362f74d4f9f5d061ace0dfd0
khlnnvarma <74981623+khlnnvarma@users.noreply.github.com>
2021-11-09 14:21:39 +0530

    ok

--- src/main/java/com/java/BcgHpRestSoapIntegrationApplication.java
+++ src/main/java/com/java/BcgHpRestSoapIntegrationApplication.java
@@ -9,12 +9,6 @@ public class BcgHpRestSoapIntegrationApplication {
 
 	public static void main(String[] args) {
 	ConfigurableApplicationContext configurableApplicationContext=	SpringApplication.run(BcgHpRestSoapIntegrationApplication.class, args);
-	
-	configurableApplicationContext.close();
-	
-	int a=1;
-	int b=2;
-	System.out.println(a+b+10);
 	}
 
 }
